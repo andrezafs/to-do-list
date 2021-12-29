ad435413bdfbc6ad3f580e75a3b4e8323d506d6b
Andreza Ferreira <andrezafs.rr@gmail.com>
2021-12-29 11:00:36 -0400

    style: remoção da pesquisa, do btndelete (duplicado) e do checkbox

--- src/js/demo.js
+++ src/js/demo.js
@@ -3,7 +3,7 @@
 const addTarefa = document.querySelector('.add-tarefa')
 let newTarefa = document.querySelector('#tarefa')
 const box = document.querySelector('.box-list');
-var input = document.getElementById('search');
+
 // const inputbox = document.querySelector('.box-new-tarefa')
 
 
@@ -16,7 +16,7 @@ addTarefa.addEventListener('click', function () {
 
 })
 
-input.addEventListener('keyup', pesquisar);
+
 
 
 
@@ -43,19 +43,13 @@ criarDelete().style.backgroundColor= 'red'
 function criarButton() {
     const button = document.createElement("button");
     button.id = `delete${(idN)}`;
-    button.classList = 'delete'
-    button.textContent = "-";
+    button.classList = 'delete';
     button.onclick = deletar;
+    button.style.backgroundImage = "url('https://img.icons8.com/ios-glyphs/30/000000/delete-forever.png')";
     return button;
 }
 
-function criarCheckBox() {
-    const checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
-    checkBox.id = 'check';
-    checkBox.onclick = aparecer;
-    return checkBox;
-}
+
 
 function aviso() {
 
@@ -73,38 +67,4 @@ function deletar() {
 
 
 }
-
-function aparecer() {
-    const chk = document.querySelector("#check");
-    const lis = document.querySelector('li');
-
-    if (chk.checked) {
-        lis.classList.add('concluido');
-
-    } else lis.classList.remove('concluido');
-
-}
-function criarDelete() {
-    const btndelete =document.createElement('button');
-    btndelete.onclick = deletar;
-    btndelete.className= 'delete';
-    btndelete.style.backgroundImage = "url('https://img.icons8.com/ios-glyphs/30/000000/delete-forever.png')";
-    return btndelete;
-}  
-
-task = document.getElementsByClassName('ul');
-searchV = document.getElementById( 'search');
-
-function pesquisar() {
-  filtrar = searchV.value;
-    for(i = 0; i <task.length; i++){
-        h5 = task[i].getElementsByTagName('li')[0];
-    
-        //Se combinarem
-        if (h5.innerHTML.indexOf(filtrar) > -1) {
-          task[i].style.display = "";
-        } else {
-         task[i].style.display = "none";
-        }
-      } 
-}
+  
